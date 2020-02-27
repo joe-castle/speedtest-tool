d36596a36fb7f556ef744ffd544049fe395bf586
Joe Smith <joesmith0488@gmail.com>
2020-02-27 14:28:57 +0000

    added push notification if speed below threshold

--- speedtest.sh
+++ speedtest.sh
@@ -2,4 +2,23 @@
 
 source $HOME/services/functions.sh
 
-log_output "$(speedtest-csv --sep , >> $HOME/speedtest.csv | echo Speedtet Run)" "$HOME/services/speedtest-tool"
+# Threshold in Mb/s
+threshold=40
+
+result=$(speedtest-csv --sep , 2>&1)
+
+if [[ $? != 0 ]]; then
+	log_output "Error running speedtest: $result" "$HOME/services/speedtest-tool"
+	exit
+fi
+
+download=$(echo $result | awk -F , '{print int($8)}')
+upload=$(echo $result | awk -F , '{print int($9)}')
+
+echo $result >> $HOME/speedtest.csv
+
+if ((download <= threshold)); then
+	log_output "Speed threshold below required, download = $download, upload: $upload." "$HOME/services/speedtest-tool"
+	curl -s -X POST -H "Content-Type: application/json" -d "{\"value1\":\"$download\",\"value2\":\"$upload\"}" \
+		 "https://maker.ifttt.com/trigger/speed_dropped/with/key/$1"
+fi
